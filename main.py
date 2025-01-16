c0fa8076d20168b87fa6e0b746464074ee2de971
TiDz <nguyentinvs123@Gmail.com>
2025-01-16 17:42:56 +0700

    done or i hope that

---
+++ main.py
@@ -0,0 +1,104 @@
+#
+# File: main.py
+# Author: TiDz
+# Contact: nguyentinvs123@gmail.com
+# Created on Thu Jan 16 2025
+#
+# Description: 
+#
+# The MIT License (MIT)
+# Copyright (c) 2025 TiDz
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+# and associated documentation files (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions: unconditional.
+#
+# Useage: 
+#
+
+import time 
+import os
+import re
+from dotenv import load_dotenv
+
+load_dotenv()
+
+logs_path = os.getenv('PATH_OF_CLIENT')
+
+whisper_pattern = re.compile(r'@From (\w+): Hi, I would like to buy your (.+) listed for (.+) in (.+)')
+whisper_bulk_pattern = re.compile(r"@From (\w+): Hi, I'd like to buy your (.+) (.+) for my (.+) Orb in Settlers")
+join_hideout_pattern = re.compile(r'(\w+) has joined the area.')
+trade_accept_pattern = re.compile(r'Trade accepted.')
+
+is_pm = False
+is_wait = False
+is_trade = False
+
+class Bot:
+    def __init__(self):
+        pass
+
+    def wait_whisper(self, line, pattern):
+        result = []
+        match = pattern.search(line)
+        if match:
+            print(f'New whisper detected:')
+            result.append(match.group(1))
+            result.append(match.group(2))
+            result.append(match.group(3))
+            result.append(match.group(4))
+            return result
+        return result
+
+    def wait_join(self, line, pattern):
+        result = []
+        match = pattern.search(line)
+        if match:
+            return True
+        return False
+
+
+if __name__ == "__main__":
+    bot = Bot()
+    with open(logs_path, 'r', encoding='utf-8') as file:
+        file.seek(0, 2)
+        while True:
+            line = file.readline()
+            if not line:
+                time.sleep(1)
+                continue
+
+            info_whisper = bot.wait_whisper(line, whisper_pattern)
+            if info_whisper and not is_pm:
+                is_pm = True
+                print(info_whisper)
+                name = info_whisper[0]
+                price = info_whisper[2]
+                pos = info_whisper[3]
+                print("command /invite " + name + " sale " + pos + " " + price)
+            
+            info_bulk_whisper = bot.wait_whisper(line, whisper_bulk_pattern)
+            if info_bulk_whisper and not is_pm:
+                is_pm = True
+                print(info_bulk_whisper)
+                name = info_bulk_whisper[0]
+                num = info_bulk_whisper[1]
+                item = info_bulk_whisper[2]
+                price = info_bulk_whisper[3]
+                print("command /invite " + name + " sale " + num + " " + item + " " + price)
+
+
+            info_join = bot.wait_join(line, join_hideout_pattern)
+            if info_join and not is_wait:
+                is_wait = True
+                print("Join done!")
+
+            info_trade = bot.wait_join(line, trade_accept_pattern)
+            if info_trade and not is_trade:
+                is_trade = True
+                print("Trade done")
+                is_pm = False
+                is_wait = False
+                is_trade = False
